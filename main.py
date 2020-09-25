90410ecd1d54760be1e03443dcd17fc96406c1c0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2020-09-24 23:45:18 -0500

    audio analysis tested + audio features

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get("CLIENT_
                                                redirect_uri="https://blog.thedojo.mx",
                                                scope="user-library-read"))
 
-results = sp.current_user_saved_tracks()
+results = sp.current_user_saved_tracks(50)
 for idx, item in enumerate(results['items']):
     track = item['track']
     print(idx, track['artists'][0]['name'], " – ", track['name'])
+    print("Analysis:" )
+    analysis = sp.audio_features(track['id'])
+    print(analysis)
