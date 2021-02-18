8cda4f2b458f97df254cc80ff707e329c21d06a4
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-02-17 23:51:03 -0600

    wip

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,3 +10,8 @@ def index():
 @app.route("/welcome_user")
 def welcome_auth_user():
     return "Auth redirect users"
+
+
+@app.route("/about")
+def about():
+    return "Servicio para que puedas consultar tus estadísticas de la música que escuchas"
