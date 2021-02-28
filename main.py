a3ae20edd35b1262075346a0464316754ec7b951
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-02-27 18:06:26 -0600

    new test

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import os
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get("CLIENT_ID"),
                                                client_secret=os.environ.get("CLIENT_SECRET"),
                                                redirect_uri="https://blog.thedojo.mx",
-                                               scope="user-library-read"))
+                                               scope="user-library-read,user-read-recently-played"))
 
-results = sp.current_user_saved_tracks(50)
+results = sp.current_user_recently_played(50)
 for idx, item in enumerate(results['items']):
     track = item['track']
     print(idx, track['artists'][0]['name'], " – ", track['name'])
     # print("Analysis:" )
     analysis = sp.audio_features(track['id'])
+# results = sp.c
+# for idx, item in enumerate(results['items']):
+#     print(idx, item["name"], item["external_urls"]["spotify"])
+#     # print("Analysis:" )
